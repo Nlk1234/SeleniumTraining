51344a7ce0c2fdc301652c6b93d0c327aface248
Dell <Dell@DESKTOP-NHC1RFQ>
2024-03-08 10:59:30 +0530

    changes

--- mavenbegin/src/test/java/Beauty/LunchSugar_Test.java
+++ mavenbegin/src/test/java/Beauty/LunchSugar_Test.java
@@ -15,7 +15,7 @@ public class LunchSugar_Test {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://in.sugarcosmetics.com/collections/sugar-x-jbl-2299?utm_source=google&utm_medium=perf_max&utm_campaign=17185724877&adgroupid=&utm_content=&utm_term=&gad_source=1&gclid=EAIaIQobChMI_szZmfPehAMVsx-DAx3howgwEAAYASAAEgJT0PD_BwE");
-		driver.findElement(By.xpath("(//div[@class=\"navbarHeadings py-3\"])[1]"));
+		driver.findElement(By.xpath("(//div[@class=\"navbarHeadings py-3\"])[1]")).click();
 		driver.quit();	
 	}
 }
